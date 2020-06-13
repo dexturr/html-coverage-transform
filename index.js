3b93c92021f1ca011f7bf272161e2a94277162d6
Dexter Edwards <dexter.edwards@cudoventures.com>
2020-06-13 23:20:28 +0100

    some test html with some components being joined on the front

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const parser = require('posthtml')
+const fs = require('fs')
+const posthtml = require('posthtml')
 
-const tree = parser(`<div></div>`)
+const text = fs.readFileSync('./test.html', { encoding: 'UTF-8' })
+const ph = posthtml()
 
-console.log(tree.walk(node => console.log(node)))
+const statementComponentName = 'test'
+let count = 0
+ph.use(function (tree) {
+  tree.walk(function (node) {
+    if (Array.isArray(node.content)) {
+      if (node.content[0].tag !== statementComponentName) {
+        node.content = node
+          .content
+          .map(t => t.tag ? [{ tag: statementComponentName }, t] : [t])
+          .reduce((acc, cur) => [...acc, ...cur], [])
+      }
+    }
+    return node
+  })
+})
+const res = ph.process(text, { sync: true })
+console.log('%o', res.tree)
+console.log(res.html)
