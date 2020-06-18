fdfa1fd03c1cee03ee41bae108b095db4311e6d0
Dexter Edwards <dexter.edwards@cudoventures.com>
2020-06-18 22:32:21 +0100

    add some instrumented code for reference purposes

---
+++ js-comparisions/a.instrumented.js
@@ -0,0 +1,35 @@
+function cov_1zt7754pmz() {
+    var path = "/Users/dexter/Documents/git/html-coverage-transform/js-comparisions/a.js"; 
+    var hash = "835b0703deb036361968fb05fd5c641bd5373a86"; 
+    var global = new Function("return this")(); 
+    var gcv = "__coverage__"; 
+    var coverageData = { 
+        path: "/Users/dexter/Documents/git/html-coverage-transform/js-comparisions/a.js", 
+        statementMap: { 
+            "0": { start: { line: 1, column: 10 }, end: { line: 1, column: 11 } }, 
+            "1": { start: { line: 2, column: 10 }, end: { line: 2, column: 11 } }, 
+            "2": { start: { line: 3, column: 10 }, end: { line: 3, column: 11 } }, 
+            "3": { start: { line: 4, column: 10 }, end: { line: 4, column: 11 } }
+        }, 
+        fnMap: {}, 
+        branchMap: {}, 
+        s: { 
+            "0": 0, 
+            "1": 0, 
+            "2": 0, 
+            "3": 0 
+        }, 
+        f: {}, 
+        b: {}, 
+        _coverageSchema: "1a1c01bbd47fc00a2c39e90264f33305004495a9", 
+        hash: "835b0703deb036361968fb05fd5c641bd5373a86" }; 
+        var coverage = global[gcv] || (global[gcv] = {}); if (!coverage[path] || coverage[path].hash !== hash) { coverage[path] = coverageData; } var actualCoverage = coverage[path]; {// @ts-ignore
+        cov_1zt7754pmz = function () { return actualCoverage; };
+    } return actualCoverage;
+}
+cov_1zt7754pmz(); 
+const a = (cov_1zt7754pmz().s[0]++, 1); 
+const b = (cov_1zt7754pmz().s[1]++, 2); 
+const c = (cov_1zt7754pmz().s[2]++, 3); 
+const d = (cov_1zt7754pmz().s[3]++, 4);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImEuanMiXSwibmFtZXMiOlsiYSIsImIiLCJjIiwiZCJdLCJtYXBwaW5ncyI6IjgyQkFlWTsyRkFmWixLQUFNQSxDQUFBQSxDQUFDLDBCQUFHLENBQUgsQ0FBUCxDQUNBLEtBQU1DLENBQUFBLENBQUMsMEJBQUcsQ0FBSCxDQUFQLENBQ0EsS0FBTUMsQ0FBQUEsQ0FBQywwQkFBRyxDQUFILENBQVAsQ0FDQSxLQUFNQyxDQUFBQSxDQUFDLDBCQUFHLENBQUgsQ0FBUCIsInNvdXJjZXNDb250ZW50IjpbImNvbnN0IGEgPSAxXG5jb25zdCBiID0gMlxuY29uc3QgYyA9IDNcbmNvbnN0IGQgPSA0Il19
---
+++ js-comparisions/b.instrumented.js
@@ -0,0 +1,36 @@
+function cov_1zt7754pmz() {
+    var path = "/Users/dexter/Documents/git/html-coverage-transform/js-comparisions/a.js";
+    var hash = "fc0fa102c9aeaa42fdb0313237ba65238ecc6e8a";
+    var global = new Function("return this")();
+    var gcv = "__coverage__";
+    var coverageData = {
+        path: "/Users/dexter/Documents/git/html-coverage-transform/js-comparisions/a.js",
+        statementMap: {
+            "0": { start: { line: 1, column: 10 }, end: { line: 1, column: 11 } },
+            "1": { start: { line: 1, column: 22 }, end: { line: 1, column: 23 } },
+            "2": { start: { line: 2, column: 10 }, end: { line: 2, column: 11 } },
+            "3": { start: { line: 3, column: 10 }, end: { line: 3, column: 11 } }
+        }, 
+        fnMap: {}, 
+        branchMap: {}, 
+        s: {
+            "0": 0, 
+            "1": 0, 
+            "2": 0, 
+            "3": 0
+        }, 
+        f: {}, 
+        b: {}, 
+        _coverageSchema: "1a1c01bbd47fc00a2c39e90264f33305004495a9", 
+        hash: "fc0fa102c9aeaa42fdb0313237ba65238ecc6e8a"
+    }; 
+    var coverage = global[gcv] || (global[gcv] = {}); if (!coverage[path] || coverage[path].hash !== hash) { coverage[path] = coverageData; } var actualCoverage = coverage[path]; {// @ts-ignore
+    cov_1zt7754pmz = function () { return actualCoverage; };
+    } return actualCoverage;
+}
+cov_1zt7754pmz();
+const a = (cov_1zt7754pmz().s[0]++, 1);
+const b = (cov_1zt7754pmz().s[1]++, 2);
+const c = (cov_1zt7754pmz().s[2]++, 3);
+const d = (cov_1zt7754pmz().s[3]++, 4);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImEuanMiXSwibmFtZXMiOlsiYSIsImIiLCJjIiwiZCJdLCJtYXBwaW5ncyI6IjgyQkFlWTsyRkFmWixLQUFNQSxDQUFBQSxDQUFDLDBCQUFHLENBQUgsQ0FBUCxDQUFZLEtBQU1DLENBQUFBLENBQUMsMEJBQUcsQ0FBSCxDQUFQLENBQ1osS0FBTUMsQ0FBQUEsQ0FBQywwQkFBRyxDQUFILENBQVAsQ0FDQSxLQUFNQyxDQUFBQSxDQUFDLDBCQUFHLENBQUgsQ0FBUCIsInNvdXJjZXNDb250ZW50IjpbImNvbnN0IGEgPSAxO2NvbnN0IGIgPSAyXG5jb25zdCBjID0gM1xuY29uc3QgZCA9IDQiXX0=
