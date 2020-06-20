40670aa7cee94a23bfe7f501484722b7f77a33a4
Dexter Edwards <dexter.edwards@cudoventures.com>
2020-06-20 23:35:22 +0100

    fix tests and invert statement map dependency

--- src/plugin.js
+++ src/plugin.js
@@ -1,8 +1,8 @@
 const statementComponentName = 't-s'
-const generateCoverageStatement = () => ({
+const generateCoverageStatement = (statementId) => ({
   tag: statementComponentName,
   attrs: {
-    ':statement-id': 1
+    ':statement-id': statementId
   }
 })
 
@@ -14,82 +14,77 @@ const hasContent = (n) => {
   return typeof n === 'object' && Array.isArray(n.content)
 }
 
-let lineNumber = 1
-let statementCount = 0
-let colNumber = 0
-let statementMap = {}
-
-const handleTag = (acc, n) => {
-  const statementId = statementCount
-  const startLine = lineNumber
-  const startCol = colNumber
-  colNumber += n.tag.length + 2
-  statementCount++
+const handleTag = (acc, n, statementMap, locationInformation) => {
+  const statementId = locationInformation.statementCount
+  const startLine = locationInformation.lineNumber
+  const startCol = locationInformation.colNumber
+  locationInformation.colNumber += n.tag.length + 2
+  locationInformation.statementCount++
 
   const result =  [
     ...acc,
-    generateCoverageStatement(),
+    generateCoverageStatement(statementId),
     n
   ]
-  colNumber += n.tag.length + 3
+  locationInformation.colNumber += n.tag.length + 3
     statementMap[statementId] = { 
       start: {
         line: startLine,
         column: startCol
       },
       end: {
-        line: lineNumber,
-        column: colNumber
+        line: locationInformation.lineNumber,
+        column: locationInformation.colNumber
       }
     }
   return result
 }
 
-const handleNewLine = (acc, n) => {
+const handleNewLine = (acc, n, locationInformation) => {
   const lineSegments = n.split('\n')
   const newLines = lineSegments.length - 1
-  lineNumber += newLines
+  locationInformation.lineNumber += newLines
   const charactersAfterNewLines = lineSegments[lineSegments.length - 1].length
-  colNumber = charactersAfterNewLines
+  locationInformation.colNumber = charactersAfterNewLines
   return [
     ...acc,
     n
   ]
 }
 
-const handleNodeWithContent = (acc, n) => {
-  const statementId = statementCount
-  const startLine = lineNumber
-  const startCol = colNumber
-  colNumber += n.tag.length + 2
-  statementCount++
+const handleNodeWithContent = (acc, n, statementMap, locationInformation) => {
+  const statementId = locationInformation.statementCount
+  const startLine = locationInformation.lineNumber
+  const startCol = locationInformation.colNumber
+  locationInformation.colNumber += n.tag.length + 2
+  locationInformation.statementCount++
   const result = [
     ...acc, 
-    generateCoverageStatement(), 
-    addCoverageStatementsForNode(n)
+    generateCoverageStatement(statementId), 
+    addCoverageStatementsForNode(n, statementMap, locationInformation)
   ]
-  colNumber += n.tag.length + 3
+  locationInformation.colNumber += n.tag.length + 3
   statementMap[statementId] = { 
     start: {
       line: startLine,
       column: startCol
     },
     end: {
-      line: lineNumber,
-      column: colNumber
+      line: locationInformation.lineNumber,
+      column: locationInformation.colNumber
     }
   }
   return result
 }
 
-const addCoverageStatementsForNode = (node) => {
+const addCoverageStatementsForNode = (node, statementMap, locationInformation) => {
   node.content = node.content.reduce((acc, n) => {
     if (hasContent(n)) {
-      return handleNodeWithContent(acc, n)
+      return handleNodeWithContent(acc, n, statementMap, locationInformation)
     } else if (isNewLine(n)) {
-      return handleNewLine(acc, n)
-    } 
-    return handleTag(acc, n)
+      return handleNewLine(acc, n, locationInformation)
+    }
+    return handleTag(acc, n, statementMap, locationInformation)
   }, [])
   return {
     ...node
@@ -97,9 +92,17 @@ const addCoverageStatementsForNode = (node) => {
 }
 
 module.exports = function (tree) {
+  const locationInformation = {
+    lineNumber: 1,
+    statementCount: 0,
+    colNumber : 0
+  }
+
+  let statementMap = {}
+
     tree.match({ tag: 'template' }, (node) => {
       if (hasContent(node)) {
-        const result = addCoverageStatementsForNode(node)
+        const result = addCoverageStatementsForNode(node, statementMap, locationInformation)
         console.log(statementMap)
         return result
       } else {
