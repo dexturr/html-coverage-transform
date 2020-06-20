2cee3e26e216fc84b88334edd757b7dc91e54957
Dexter Edwards <dexter.edwards@cudoventures.com>
2020-06-20 23:15:56 +0100

    location information for basic things

--- src/plugin.js
+++ src/plugin.js
@@ -1,6 +1,6 @@
-const statementComponentName = 'test'
+const statementComponentName = 't-s'
 const generateCoverageStatement = () => ({
-  tag: 't-s',
+  tag: statementComponentName,
   attrs: {
     ':statement-id': 1
   }
@@ -14,26 +14,57 @@ const hasContent = (n) => {
   return typeof n === 'object' && Array.isArray(n.content)
 }
 
+let lineNumber = 1
+let statementCount = 0
+let colNumber = 0
+let statementMap = {}
+
 const addCoverageStatementsForNode = (node) => {
   node.content = node.content.reduce((acc, n) => {
     if (hasContent(n)) {
+      statementCount++
+      // console.log(lineNumber, statementCount, colNumber, 's')
       return [
         ...acc, 
         generateCoverageStatement(), 
         addCoverageStatementsForNode(n)
       ]
     } else if (isNewLine(n)) {
+      const lineSegments = n.split('\n')
+      const newLines = lineSegments.length - 1
+      lineNumber += newLines
+      const charactersAfterNewLines = lineSegments[lineSegments.length - 1].length
+      colNumber += charactersAfterNewLines
       return [
         ...acc,
         n
       ]
-    }
-    return [
+    } 
+    const statementId = statementCount
+    const startLine = lineNumber
+    const startCol = colNumber
+    colNumber += n.tag.length + 2
+    statementCount++
+
+    const result =  [
       ...acc,
       generateCoverageStatement(),
       n
     ]
+    colNumber += n.tag.length + 3
+      statementMap[statementId] = { 
+        start: {
+          line: startLine,
+          column: startCol
+        },
+        end: {
+          line: lineNumber,
+          column: colNumber
+        }
+      }
+    return result
   }, [])
+  console.log(statementMap)
   return {
     ...node
   }
@@ -43,9 +74,8 @@ module.exports = function (tree) {
     tree.match({ tag: 'template' }, (node) => {
       if (hasContent(node)) {
         return addCoverageStatementsForNode(node)
-      }
-      return {
-        ...node
+      } else {
+        throw new Error('Template tag could not be parsed as it has no content')
       }
     })
   }
