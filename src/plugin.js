2a16bf8073ee30c3e9a796f70f9d85d29abf2a44
Dexter Edwards <dexter.edwards@cudoventures.com>
2020-06-20 23:17:55 +0100

    location support for nested items

--- src/plugin.js
+++ src/plugin.js
@@ -19,52 +19,78 @@ let statementCount = 0
 let colNumber = 0
 let statementMap = {}
 
+const handleTag = (acc, n) => {
+  const statementId = statementCount
+  const startLine = lineNumber
+  const startCol = colNumber
+  colNumber += n.tag.length + 2
+  statementCount++
+
+  const result =  [
+    ...acc,
+    generateCoverageStatement(),
+    n
+  ]
+  colNumber += n.tag.length + 3
+    statementMap[statementId] = { 
+      start: {
+        line: startLine,
+        column: startCol
+      },
+      end: {
+        line: lineNumber,
+        column: colNumber
+      }
+    }
+  return result
+}
+
+const handleNewLine = (acc, n) => {
+  const lineSegments = n.split('\n')
+  const newLines = lineSegments.length - 1
+  lineNumber += newLines
+  const charactersAfterNewLines = lineSegments[lineSegments.length - 1].length
+  colNumber = charactersAfterNewLines
+  return [
+    ...acc,
+    n
+  ]
+}
+
+const handleNodeWithContent = (acc, n) => {
+  const statementId = statementCount
+  const startLine = lineNumber
+  const startCol = colNumber
+  colNumber += n.tag.length + 2
+  statementCount++
+  const result = [
+    ...acc, 
+    generateCoverageStatement(), 
+    addCoverageStatementsForNode(n)
+  ]
+  colNumber += n.tag.length + 3
+  statementMap[statementId] = { 
+    start: {
+      line: startLine,
+      column: startCol
+    },
+    end: {
+      line: lineNumber,
+      column: colNumber
+    }
+  }
+  return result
+}
+
 const addCoverageStatementsForNode = (node) => {
   node.content = node.content.reduce((acc, n) => {
     if (hasContent(n)) {
-      statementCount++
-      // console.log(lineNumber, statementCount, colNumber, 's')
-      return [
-        ...acc, 
-        generateCoverageStatement(), 
-        addCoverageStatementsForNode(n)
-      ]
+      return handleNodeWithContent(acc, n)
     } else if (isNewLine(n)) {
-      const lineSegments = n.split('\n')
-      const newLines = lineSegments.length - 1
-      lineNumber += newLines
-      const charactersAfterNewLines = lineSegments[lineSegments.length - 1].length
-      colNumber += charactersAfterNewLines
-      return [
-        ...acc,
-        n
-      ]
+      return handleNewLine(acc, n)
     } 
-    const statementId = statementCount
-    const startLine = lineNumber
-    const startCol = colNumber
-    colNumber += n.tag.length + 2
-    statementCount++
-
-    const result =  [
-      ...acc,
-      generateCoverageStatement(),
-      n
-    ]
-    colNumber += n.tag.length + 3
-      statementMap[statementId] = { 
-        start: {
-          line: startLine,
-          column: startCol
-        },
-        end: {
-          line: lineNumber,
-          column: colNumber
-        }
-      }
-    return result
+    return handleTag(acc, n)
   }, [])
-  console.log(statementMap)
   return {
     ...node
   }
@@ -73,7 +99,9 @@ const addCoverageStatementsForNode = (node) => {
 module.exports = function (tree) {
     tree.match({ tag: 'template' }, (node) => {
       if (hasContent(node)) {
-        return addCoverageStatementsForNode(node)
+        const result = addCoverageStatementsForNode(node)
+        console.log(statementMap)
+        return result
       } else {
         throw new Error('Template tag could not be parsed as it has no content')
       }
